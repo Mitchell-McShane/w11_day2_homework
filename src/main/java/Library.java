6aba629bccd23e8571e2569a04d3ebb8648a40f2
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2021-01-26 17:20:07 +0000

    count method works with test function passing

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private int capacity;
+    private ArrayList<Book> books;
+
+    public Library(int capacity){
+        this.capacity = capacity;
+        this.books = new ArrayList<>();
+    }
+
+    public int getBookCount(){
+        return this.books.size();
+    }
 }
